42551d5b9893958ccd54d01a0932d6028cbc1c52
S R Jay <78264191+srjaykikani@users.noreply.github.com>
2024-11-27 01:21:43 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export async function middleware(req: NextRequest) {
     ['/sign-in', '/sign-up'].includes(nextUrl.pathname)
   ) {
     return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/`
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`
     )
   }
 
